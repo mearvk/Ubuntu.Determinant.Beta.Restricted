27f21df142dfed0b433443a7e73d16ad64d689b4
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-10 23:25:14 -0400

    tac3: share on-disk format definitions with kernel

--- tools/tac3/tac3_format.hpp
+++ tools/tac3/tac3_format.hpp
@@ -1,40 +1,26 @@
 #ifndef TAC3_FORMAT_HPP
 #define TAC3_FORMAT_HPP
-#include <cstdint>
+
 #include <cstddef>
+#include <cstdint>
+
+extern "C" {
+#include "../../file-systems/tac3/tac3_format.h"
+}
+
 namespace tac3 { namespace format {
-constexpr std::size_t block_size = 4096;
-constexpr std::size_t magic_size = 8;
+constexpr std::size_t block_size = TAC3_DISK_BLOCK_SIZE;
+constexpr std::size_t magic_size = TAC3_DISK_MAGIC_SIZE;
 constexpr char magic[magic_size] = {'T','A','C','3','F','S','1','\0'};
-constexpr std::uint16_t major = 1;
-constexpr std::uint16_t minor = 0;
-constexpr std::uint32_t checksum_crc32c = 1;
-constexpr std::uint32_t state_clean = 0;
-constexpr std::uint32_t state_active = 1;
-constexpr std::uint32_t state_recovery_required = 2;
-constexpr std::uint64_t minimum_blocks = 64;
+constexpr std::uint16_t major = TAC3_DISK_FORMAT_MAJOR;
+constexpr std::uint16_t minor = TAC3_DISK_FORMAT_MINOR;
+constexpr std::uint32_t checksum_crc32c = TAC3_DISK_CHECKSUM_CRC32C;
+constexpr std::uint32_t state_clean = TAC3_DISK_STATE_CLEAN;
+constexpr std::uint32_t state_active = TAC3_DISK_STATE_ACTIVE;
+constexpr std::uint32_t state_recovery_required = TAC3_DISK_STATE_RECOVERY_REQUIRED;
+constexpr std::uint64_t minimum_blocks = TAC3_DISK_MIN_BLOCKS;
 constexpr std::uint32_t default_multitude = 10;
 constexpr std::uint32_t default_device_class = 5;
-struct SuperblockFields {
-    std::uint16_t format_major = major;
-    std::uint16_t format_minor = minor;
-    std::uint32_t block_size = 4096;
-    std::uint64_t total_blocks = 0;
-    std::uint8_t uuid[16]{};
-    std::uint64_t generation = 1;
-    std::uint32_t multitude = default_multitude;
-    std::uint32_t device_class = default_device_class;
-    std::uint64_t file_table_start = 0;
-    std::uint64_t file_table_blocks = 0;
-    std::uint64_t health_table_start = 0;
-    std::uint64_t health_table_blocks = 0;
-    std::uint64_t admin_table_start = 0;
-    std::uint64_t admin_table_blocks = 0;
-    std::uint64_t recovery_start = 0;
-    std::uint64_t recovery_blocks = 0;
-    std::uint64_t data_start = 0;
-    std::uint32_t state = state_clean;
-    std::uint32_t checksum_algorithm = checksum_crc32c;
-};
 }}
+
 #endif
